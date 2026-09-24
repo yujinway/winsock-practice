679e5e661da9b7f5d14341512c57018aa67cbd07
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-09-24 21:16:23 +0900

    Initializing Winsock

--- WinsockClient/WinsockClient.cpp
+++ WinsockClient/WinsockClient.cpp
@@ -6,5 +6,16 @@
 
 int main(int argc, char* argv[])
 {
+    // Initialize Winsock
+    WSADATA wsaData;
+
+    int iResult;
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
     return 0;
 }
