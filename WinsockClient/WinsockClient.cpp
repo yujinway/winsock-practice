968d4d8781bd8c29fc00c04b1956d1048f688a01
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-09-24 21:21:41 +0900

    Creating a socket for the client

--- WinsockClient/WinsockClient.cpp
+++ WinsockClient/WinsockClient.cpp
@@ -4,9 +4,11 @@
 
 #pragma comment(lib, "Ws2_32.lib");
 
+#define DEFAULT_PORT "27015"
+
 int main(int argc, char* argv[])
 {
-    // Initialize Winsock
+    // Winsock 초기화
     WSADATA wsaData;
 
     int iResult;
@@ -14,6 +16,39 @@ int main(int argc, char* argv[])
     if (iResult != 0)
     {
         printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    struct addrinfo *result = NULL,
+                *ptr = NULL,
+                hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // 서버 주소와 포트 번호 조회
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    if (iResult != 0)
+    {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+    // getaddrinfo가 반환한 첫 번째 주소로 연결 시도
+    ptr = result;
+
+    // 서버에 연결할 소켓 생성
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET)
+    {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
         return 1;
     }
 
