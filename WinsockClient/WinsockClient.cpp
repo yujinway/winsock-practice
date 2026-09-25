4731610e82cb6ad20c3277dd6534eeeb68465cef
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-09-25 15:29:30 +0900

    Sending and receiving data on the client

--- WinsockClient/WinsockClient.cpp
+++ WinsockClient/WinsockClient.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib, "Ws2_32.lib");
 
 #define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
 
 int main(int argc, char* argv[])
 {
@@ -59,7 +60,7 @@ int main(int argc, char* argv[])
         closesocket(ConnectSocket);
         ConnectSocket = INVALID_SOCKET;
     }
-    
+
     // getaddrinfo는 서버 주소에 해당하는 연결 후보들을 연결 리스트로 반환한다.
     // 첫 번째 주소로 connect에 실패하면 다음 주소도 시도할 수 있다.
     // 이 예제에서는 재시도하지 않고 결과를 해제한 뒤 오류 메시지를 출력한다.
@@ -71,6 +72,54 @@ int main(int argc, char* argv[])
         WSACleanup();
         return 1;
     }
+
+    int recvbuflen = DEFAULT_BUFLEN;;
+
+    const char* sendbuf = "this is a test";
+    char recvbuf[DEFAULT_BUFLEN];
+
+    // 버퍼에 담긴 문자열을 서버에 전송
+    iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    // 더 이상 전송할 데이터가 없으므로 연결의 송신을 종료
+    // 송신을 종료한 후에도 ConnectSocket을 사용하여 데이터 수신이 가능하다
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("shutdown failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // 서버가 연결을 종료할 때까지 데이터를 수신
+    do
+    {
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+
+        if (iResult > 0)
+        {
+            printf("Bytes received: %d\n", iResult);
+        }
+        else if (iResult == 0)
+        {
+            printf("Connection closed\n");
+        }
+        else
+        {
+            printf("recv failed: %d\n", WSAGetLastError());
+        }
+    }
+    while (iResult > 0);
 
     return 0;
 }
